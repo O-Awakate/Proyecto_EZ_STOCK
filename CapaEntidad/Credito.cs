04839d8bb8c865375f17c0a1f6558c4a1f2534ba
O-Awakate <148908286+O-Awakate@users.noreply.github.com>
2023-11-21 11:12:19 -0400

    Cambios

---
+++ CapaEntidad/Credito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Credito
+    {
+        public int IdCompraCredito { get; set; }
+        public string FechaVencimiento { get; set; }
+        public decimal Intereses { get; set; }
+        public decimal Deuda { get; set; }
+        public string FechaRegistro { get; set; }
+    }
+}
